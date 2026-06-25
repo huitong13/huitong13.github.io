83d23970770fd9ec0dc6c9f63354182d801d0614
huitong13 <1639276089@qq.com>
2026-06-25 15:02:32 +0800

    Update data.js

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.dashboardData = {
-    date: "2026-06-25",
+    date: "2026-06-26",
     activeJobs: 30,
     resumeHigh: 162,
     resumeLow: 299,
@@ -9,6 +9,7 @@ window.dashboardData = {
         { name: "邀约面试中", high: 0, low: 2 },
         { name: "已安排一面", high: 13, low: 4 },
         { name: "一面通过",   high: 7, low: 2 },
+        { name: "一面不通过", high: 0, low: 0 },  
         { name: "待一面",     high: 2, low: 0 },
         { name: "安排二面",   high: 3, low: 2 },
         { name: "二面通过",   high: 1, low: 0 },
